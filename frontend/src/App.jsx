302b2e85bddc78c25147c84930872d1e4d54fc37
amyy45 <itssneha45@gmail.com>
2025-04-01 23:18:41 +0530

    update 3.2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+       <div className='text-5xl font-bold text-red-500'>Hello there</div>
+    </>
+  )
+}
+
+export default App
