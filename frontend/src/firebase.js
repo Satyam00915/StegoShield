c60759a7701e2b50593c52e71e22bb4af20ad272
amyy45 <itssneha45@gmail.com>
2025-04-06 21:30:03 +0530

    api key

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,21 +1,20 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider,signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQqvDYNSMWD5Pkm7e8Pf7cN5ujhZy6NqE",
-    authDomain: "stego-shield.firebaseapp.com",
-    projectId: "stego-shield",
-    storageBucket: "stego-shield.appspot.com",
-    messagingSenderId: "951143754468",
-    appId: "1:951143754468:web:bf42b3e8b3b44b8b1ddaf1",
-    measurementId: "G-90T3VFKP80"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithPopup  };
+export { auth, provider, signInWithPopup };
